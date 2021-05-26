f63e7687da3dcd77052d5f957ed0432f89097132
Max MacMillan <max@alley.co>
2021-05-26 16:25:43 -0700

    Controlled form input via useState

---
+++ src/exercise/06-controlledForm.js
@@ -0,0 +1,40 @@
+// Basic Forms
+// http://localhost:3000/isolated/exercise/06.js
+
+import React, { useState } from 'react'
+
+function UsernameForm({onSubmitUsername}) {
+  const [username, setUsername] = useState();
+
+  const handleChange = (event) => {
+    const value = event.target.value.toLowerCase();
+    setUsername(value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSubmitUsername(username);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          name="username"
+          type="text"
+          onChange={handleChange}
+          value={username}
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  )
+}
+
+function App() {
+  const onSubmitUsername = username => alert(`You entered: ${username}`)
+  return <UsernameForm onSubmitUsername={onSubmitUsername} />
+}
+
+export default App
